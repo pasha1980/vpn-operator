fd215d4c1b56da581cbc892cc7842fe5abbdd71e
pashka <hvalyginpavel@gmail.com>
2022-11-21 17:25:42 +0200

    Fix dependency circle

--- config/auth.go
+++ config/auth.go
@@ -2,11 +2,12 @@ package config
 
 import (
 	"log"
-	"vpn-operator/domain/infrastructure/auth"
+	"vpn-operator/domain/infrastructure/utils"
 )
 
 func initAuth() {
-	err := auth.GenerateNewApiKey()
+	newToken := utils.GenerateRandomString(500)
+	_, err := Redis.Set(Context, "secret_key", newToken, 0).Result()
 	if err != nil {
 		log.Fatal(err)
 	}
